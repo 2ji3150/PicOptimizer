c602633f451e422a11e6693ee27f1cd8772d7077
Syun Segawa <2ji3150@gmail.com>
2018-07-01 07:24:14 +0900

    mozjpegモードだけ実装してみました

--- PicOptimizer/MainWindow.xaml.cs
+++ PicOptimizer/MainWindow.xaml.cs
@@ -18,101 +18,92 @@ namespace PicOptimizer {
         const string mozjpeg = @"tools\jpegtran-static", mozjpeg_sw = "-copy all";
         const string winrar = @"tools\Rar", winrar_sw = "a -m5 -md1024m -ep1 -r -idq";
         const string senvenzip = @"tools\7z", senvenzip_sw = "x";
-        readonly HashSet<string> ext_mozjpg = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".jpg", ".jpeg" };
-        readonly HashSet<string> ext_cwebp = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".bmp", ".png", ".tif", "tiff", ".webp" };
-        readonly HashSet<string> ext_dwebp = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".webp" };
-        readonly HashSet<string> ext_archive = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".zip", ".rar", ".7z" };
-        SemaphoreSlim sem = new SemaphoreSlim(Environment.ProcessorCount / 2);
+        readonly HashSet<string>[] exts = new HashSet<string>[] {
+            new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".jpg", ".jpeg" },
+            new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".bmp", ".png", ".tif", "tiff", ".webp" },
+            new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".webp" },
+            new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".zip", ".rar", ".7z" }
+        };
+
         Stopwatch sw = new Stopwatch();
         private void Window_DragEnter(object sender, DragEventArgs e) => e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
         private async void Window_Drop(object sender, DragEventArgs e) {
             ViewModel vm = (ViewModel)DataContext;
             vm.Idle.Value = false;
             sw.Restart();
-            string[] dropdata = (string[])e.Data.GetData(DataFormats.FileDrop);
-            Task[] tasks;
-            List<Task> tasklist = new List<Task>();
-            long totaldelta = 0;
-            int counter = 0;
+
+            //Create TMP Dir
             string tmp_now = Path.Combine("TEMP", DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
             Directory.CreateDirectory(tmp_now);
 
-
-            #region ローカル関数
-            long Replace(ref long td, string file, string tempfile, string ext) {
+            long totaldelta = 0;
+            int counter = 0;
+            var inputBlock = new ActionBlock<(FileInfo fi, int index)>(async file => {
+                string outf = Path.Combine(tmp_now, file.index.ToString());
+                await RunProcessAsync(mozjpeg, $"{mozjpeg_sw} -outfile {outf.WQ()} {file.fi.FullName.WQ()}");
                 try {
-                    FileInfo fiI = new FileInfo(file) { IsReadOnly = false }, fiT = new FileInfo(tempfile);
+                    FileInfo fiT = new FileInfo(outf);
                     if (fiT.Length <= 0) throw new Exception("生成したファイルが破損");
-                    long delta = fiI.Length - fiT.Length;
-                    fiI.Delete();
-                    fiT.MoveTo(Path.ChangeExtension(file, ext));
-                    if (delta == 0) return td;
-                    else return Interlocked.Add(ref td, delta);
+                    long delta = file.fi.Length - fiT.Length;
+                    file.fi.IsReadOnly = false;
+                    file.fi.Delete();
+                    fiT.MoveTo(Path.ChangeExtension(file.fi.FullName, ".jpg"));
+                    if (delta != 0) {
+                        Interlocked.Add(ref totaldelta, delta);
+                    }
                 } catch (Exception ex) {
-                    MessageBox.Show($"エラー! {file} ： {ex.Message}");
-                    return td;
+                    MessageBox.Show($"エラー! {file.fi.FullName} ： {ex.Message}");
                 }
-            }
+                Interlocked.Increment(ref counter);
+                vm.Update(totaldelta, counter);
+            }, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = 5 });
 
-            IEnumerable<string> GetFiles(HashSet<string> exts, string[] data) {
-                foreach (string d in data) {
-                    if (File.GetAttributes(d).HasFlag(FileAttributes.Directory)) {
-                        foreach (string f in Directory.EnumerateFiles(d, "*.*", SearchOption.AllDirectories).Where(f => exts.Contains(Path.GetExtension(f)))) yield return f;
-                    } else if (exts.Contains(Path.GetExtension(d))) yield return d;
-                }
-            }
 
-            async Task TaskAsync(string exe, string arg) {
-                await sem.WaitAsync();
-                try {
-                    await RunProcessAsync(exe, arg);
-                } finally {
-                    sem.Release();
-                }
-            }
-            #endregion
+
 
             int i = 0;
-            var MozjpegAction = new TransformBlock<string,long>(async file => {
-                string outf = Path.Combine(tmp_now, i++.ToString());
-                await RunProcessAsync(mozjpeg, $"{mozjpeg_sw} -outfile {outf.WQ()} {file.WQ()}");
+            bool checkext(string ext) => exts[vm.Index.Value].Contains(ext);
+            foreach (string data in (string[])e.Data.GetData(DataFormats.FileDrop)) {
+                if (File.GetAttributes(data).HasFlag(FileAttributes.Directory))
+                    foreach (FileInfo fi in new DirectoryInfo(data).EnumerateFiles("*.*", SearchOption.AllDirectories).AsParallel().Where(fi => checkext(fi.Extension))) inputBlock.Post((fi, ++i));
+                else {
+                    FileInfo fi = new FileInfo(data);
+                    if (checkext(fi.Extension)) inputBlock.Post((fi, ++i));
+                }
+            }
+            inputBlock.Complete();
+
+
+            //setting ui
+            if (!vm.Start(i)) {
+                SystemSounds.Asterisk.Play();
+                return;
+            }
+            await inputBlock.Completion;
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+            SystemSounds.Asterisk.Play();
+            MessageBox.Show($"完成しました\n\n処理にかかった時間 = {ts.Hours} 時間 {ts.Minutes} 分 {ts.Seconds} 秒 {ts.Milliseconds} ミリ秒");
+            vm.Idle.Value = true;
+
+            /*
+            var replaceBlock = new ActionBlock<string>(async file => {
                 FileInfo fiI = new FileInfo(file) { IsReadOnly = false }, fiT = new FileInfo(outf);
                 if (fiT.Length <= 0) throw new Exception("生成したファイルが破損");
                 fiI.Delete();
                 fiT.MoveTo(Path.ChangeExtension(file, ".jpg"));
+
                 return fiI.Length - fiT.Length;
             });
+            */
 
-            var CwebpAction = new ActionBlock<string>(async file => {
-                string outf = Path.Combine(tmp_now, i++.ToString());
-                await RunProcessAsync(cwebp, $"{cwebp_sw} {file.WQ()} -o {outf.WQ()}");
-            });
 
-            var DwebpAction = new ActionBlock<string>(async file => {
-                string outf = Path.Combine(tmp_now, i++.ToString());
-                await RunProcessAsync(dwebp, $"{dwebp_sw} {file.WQ()} -o {outf.WQ()}");
-            });
-
-            var DecompressAction = new ActionBlock<string>(async file => {
-                string tmp_a = Path.Combine(tmp_now, i++.ToString());
-                DirectoryInfo di_a = Directory.CreateDirectory(tmp_a);
-                await RunProcessAsync(senvenzip, $"{senvenzip_sw} {file} -o{di_a.FullName.WQ()}");
-            });
-
-            var CompressAction = new ActionBlock<string>(async file => {
-                string tmp_a = Path.Combine(tmp_now, i++.ToString());
-                DirectoryInfo di_a = Directory.CreateDirectory(tmp_a);
-                string outa = tmp_a + ".rar";
-                await RunProcessAsync(winrar, $"{winrar_sw} {outa.WQ()} {(topdir + @"\").WQ()}");
-            });
-
-            
-
+            /*
 
             switch (vm.Index.Value) {
                 default://MozJpeg
-                    GetFiles(ext_mozjpg, dropdata).Select(f => MozjpegAction.Post(f));
-                    MozjpegAction.Complete();
+                    GetFiles(ext_mozjpg, dropdata).Select(f => jpgBlock.Post(f));
+                    jpgBlock.Complete();
                     tasks = GetFiles(ext_mozjpg, dropdata).Select((item, index) => (item, index)).Select(x => {
                         string outf = Path.Combine(tmp_now, x.index.ToString());
                         return TaskAsync(mozjpeg, $"{mozjpeg_sw} -outfile {outf.WQ()} {x.item.WQ()}").ContinueWith(_ => vm.Update(Replace(ref totaldelta, x.item, outf, ".jpg"), Interlocked.Increment(ref counter)));
@@ -171,6 +162,8 @@ namespace PicOptimizer {
             SystemSounds.Asterisk.Play();
             MessageBox.Show($"完成しました\n\n処理にかかった時間 = {ts.Hours} 時間 {ts.Minutes} 分 {ts.Seconds} 秒 {ts.Milliseconds} ミリ秒");
             vm.Idle.Value = true;
+           */
+
         }
 
 #if DEBUG
